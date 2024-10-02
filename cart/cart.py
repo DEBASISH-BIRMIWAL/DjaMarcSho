3e8f1f11a2ac70443131cef0c6e9c44111e36e97
debasish <debasishbirmiwal1@gmail.com>
2024-10-03 00:34:07 +0530

    Added update feature in cart

--- cart/cart.py
+++ cart/cart.py
@@ -19,6 +19,7 @@ class Cart:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
+        self.save()
 
     def save(self):
         self.session.modified = True
